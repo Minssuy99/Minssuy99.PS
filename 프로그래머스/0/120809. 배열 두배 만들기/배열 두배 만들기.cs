a35536029bc6b91c5166283aa2174dab3015eab9
김민성 <101568505+Minssuy99@users.noreply.github.com>
2024-04-30 12:38:48 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.15 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution
+{
+    public int[] solution(int[] numbers)
+    {
+        int[] answer = numbers;
+        
+        
+        for (int i = 0; i < answer.Length; i++)
+        {
+            answer[i] *= 2;
+        }
+        
+        return answer;
+    }
+}
