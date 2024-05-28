7c1aa5b0c1dfa7d83b820eafd7d416cba3fc86ce
김민성 <101568505+Minssuy99@users.noreply.github.com>
2024-05-29 02:36:33 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double A, B;
+    cin >> A;
+    cin >> B;
+    
+    cout << fixed;
+	cout.precision(9);
+    cout << A / B << endl;
+
+    return 0;
+}
