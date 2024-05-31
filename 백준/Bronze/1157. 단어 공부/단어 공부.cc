296bad49d10c5952b8337d2804b508937f7686d0
김민성 <101568505+Minssuy99@users.noreply.github.com>
2024-05-31 15:31:11 +0900

    [Bronze I] Title: 단어 공부, Time: 44 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int rank = 0;
+    int Alphabet[26] = {};
+    int Alphabet_size = sizeof(Alphabet) / sizeof(Alphabet[0]);
+    char c;
+    
+    string s;
+
+    cin >> s;
+
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= 97 && s[i] <= 122)
+        {
+            s[i] = s[i] - 32;
+
+        }
+            Alphabet[s[i] - 'A']++;
+    }
+
+    // 가장 많이 사용한 알파벳 찾기
+    for (int i = 0; i < Alphabet_size; i++)
+    {
+        if (Alphabet[i] > rank)
+        {
+            rank = Alphabet[i];
+            c = 'A' + i;
+        }
+        else if (rank == Alphabet[i])
+        {
+            c = '?';
+        }
+    }
+
+    cout << c << endl;
+    
+    return 0;
+}
