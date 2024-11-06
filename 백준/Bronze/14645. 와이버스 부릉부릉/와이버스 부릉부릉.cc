9763dea8a84e6504ae527a06c56a2b0e73aa1384
김민성 <101568505+Minssuy99@users.noreply.github.com>
2024-11-06 22:43:15 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -4,19 +4,6 @@ using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n, k;
-
-    cin >> n >> k;
-
-    for (int i = 0; i < n; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-    }
-
     cout << "비와이" << endl;
 
     return 0;
