9d17be694d99377fe376190d8db7f8d90ee19a3a
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-02-20 15:17:37 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -8,44 +8,33 @@ int main()
     cin.tie(0);
 
     int arr[9];
-    int sum = 0;
-    int num = 0;
 
     for (int i = 0; i < 9; i++)
     {
         cin >> arr[i];
-
-        sum += arr[i];
-    }
-
-    num = sum - 100;
-
-    for (int i = 0; i < 8; i++)
-    {
-        bool check = false;
-        for (int j = i + 1; j < 9; j++)
-        {
-            if (arr[i] + arr[j] == num)
-            {
-                arr[i] = 0;
-                arr[j] = 0;
-                check = true;
-                break;
-            }
-        }
-
-        if (check)
-            break;
     }
 
     sort(arr, arr + 9);
 
-    for (int i = 0; i < 9; i++)
+    do
     {
-        if (arr[i] != 0)
+        int sum = 0;
+
+        for (int i = 0; i < 7; i++)
         {
-            cout << arr[i] << '\n';
+            sum += arr[i];
         }
+
+        if (sum == 100)
+        {
+            break;
+        }
+
+    } while (next_permutation(arr, arr + 9));
+
+    for (int i = 0; i < 7; i++)
+    {
+        cout << arr[i] << '\n';
     }
 
     return 0;
