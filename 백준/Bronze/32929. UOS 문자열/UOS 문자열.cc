dc54296737a865c468aac3bee4c0e921807a5428
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-05-03 07:43:51 +0900

    [Bronze V] Title: UOS 문자열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32929. UOS 문자열/UOS 문자열.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long x;
+    cin >> x;
+
+    if (x % 3 == 1)
+        cout << 'U';
+    else if (x % 3 == 2)
+        cout << 'O';
+    else
+        cout << 'S';
+
+    return 0;
+}
