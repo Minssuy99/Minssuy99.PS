d36360f5afaf0987da35e2f1ddb571ff984fd914
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-05-17 10:34:42 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string id, fan;
+    fan = ":fan:";
+    cin >> id;
+    
+    cout << fan << fan << fan << endl;
+    cout << fan << ":" << id << ":" << fan << endl;
+    cout << fan << fan << fan;
+    
+    return 0;
+}
