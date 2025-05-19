fdc671ac274df3b57477d4dd74cc74de20a1c3da
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-05-20 06:13:34 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33612. 피갤컵/피갤컵.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int month = 24288;
+int N;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> N;
+
+    month += N * 7;
+    
+    cout << (month / 12) << " " << (month % 12 + 1) << '\n';
+}
