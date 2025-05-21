bee77a32b04ae87897abe0f2f30ef047472566b0
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-05-21 14:52:34 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int ret = 0;
+	int input;
+	for(int i=0; i<5; i++)
+    {
+		cin >> input;
+		ret += input;
+	}
+	
+	cout << ret;
+	
+	return 0;
+}
