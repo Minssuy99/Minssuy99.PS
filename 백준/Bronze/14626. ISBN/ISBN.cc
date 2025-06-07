ab004bd05fa5f08fec1322ed9ae707eb22fea381
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-06-08 00:00:50 +0900

    [Bronze I] Title: ISBN, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/14626. ISBN/ISBN.cc
+++ 백준/Bronze/14626. ISBN/ISBN.cc
@@ -1,38 +1,30 @@
 #include <bits/stdc++.h>
-#define endl '\n'
-
 using namespace std;
-
-int ret;
-string s;
 
 int main()
 {
+    string s;
     cin >> s;
 
     int idx = s.find('*');
+    int total = 0;
+
+    for (int i = 0; i < 13; i++)
+    {
+        if (i == idx) continue;
+        int digit = s[i] - '0';
+        total += (i % 2 == 0) ? digit : digit * 3;
+    }
 
     for (int i = 0; i < 10; i++)
     {
-        s[idx] = i + '0';
-        ret = 0;
-
-        for (int j = 0; j < 13; j++)
+        int temp = total + ((idx % 2 == 0) ? i : i * 3);
+        if (temp % 10 == 0)
         {
-            if (j % 2 == 1)
-            {
-                ret += (s[j] - '0') * 3;
-            }
-            else
-            {
-                ret += (s[j] - '0');
-            }
-        }
-        if (ret % 10 == 0)
-        {
-            cout << s[idx];
+            cout << i;
             break;
         }
     }
+
     return 0;
 }
