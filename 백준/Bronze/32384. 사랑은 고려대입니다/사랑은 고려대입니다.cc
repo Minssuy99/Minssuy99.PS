a29e2a93a60f16de569a3445e461e3cbc3539914
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-07-03 02:56:14 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    for (int i = 0; i < N; ++i)
+    {
+        cout << "LoveisKoreaUniversity";
+        if (i != N - 1) cout << " ";
+    }
+    cout << '\n';
+    return 0;
+}
