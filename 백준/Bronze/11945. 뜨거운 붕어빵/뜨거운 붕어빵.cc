4e81b88329ea57b549491ff447d69942d26c0024
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-07-03 22:12:29 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    string line;
+    
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> line;
+        reverse(line.begin(), line.end());
+        cout << line << '\n';
+    }
+
+    return 0;
+}
