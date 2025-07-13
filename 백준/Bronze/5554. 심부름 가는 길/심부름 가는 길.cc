fa3b8ff37ae6bb52d9d78fce38e5e34f4f5dd814
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-07-14 01:36:42 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+
+	cin >> a >> b >> c >> d;
+
+	cout << (a + b + c + d) / 60 << endl;
+	cout << (a + b + c + d) % 60 << endl;
+
+	return 0;
+}
