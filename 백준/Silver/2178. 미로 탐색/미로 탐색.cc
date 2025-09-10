e8dc6efec981c7d49a01785a2b8dc1bfa00d93bf
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-09-10 10:43:43 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2236 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int n, m;
 int arr[101][101];
 int visited[101][101];
-
-queue<pair<int, int>> q;
 
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
@@ -13,26 +12,26 @@ int dx[] = {0, 1, 0, -1};
 void bfs(int y, int x)
 {
     visited[y][x] = 1;
+
+    queue<pair<int, int>> q;
     q.push({y, x});
 
     while (!q.empty())
     {
         auto cur = q.front();
         q.pop();
-        int cy = cur.first;
-        int cx = cur.second;
 
         for (int i = 0; i < 4; i++)
         {
-            int ny = cy + dy[i];
-            int nx = cx + dx[i];
+            int ny = cur.first + dy[i];
+            int nx = cur.second + dx[i];
 
             if (ny < 0 || nx < 0 || ny >= n || nx >= m)
                 continue;
             if (arr[ny][nx] == 0 || visited[ny][nx])
                 continue;
 
-            visited[ny][nx] = visited[cy][cx] + 1;
+            visited[ny][nx] = visited[cur.first][cur.second] + 1;
             q.push({ny, nx});
         }
     }
@@ -49,14 +48,15 @@ int main()
     {
         string s;
         cin >> s;
+
         for (int j = 0; j < m; j++)
         {
             arr[i][j] = s[j] - '0';
         }
     }
-
     bfs(0, 0);
 
-    cout << visited[n - 1][m - 1] << "\n";
+    cout << visited[n - 1][m - 1];
+
     return 0;
 }
