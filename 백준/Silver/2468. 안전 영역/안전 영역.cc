5788607cca7c7e739471986afbc10a193239352d
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-09-13 18:24:01 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
+#define endl '\n'
 
 using namespace std;
 
-int area[101][101];
+int n, m_height, ret;
+
+int arr[101][101];
 bool visited[101][101];
 
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
 
-int n, max_height, area_point, ret;
-
 void bfs(int y, int x, int height)
 {
-    queue<pair<int, int>> q;
-
     visited[y][x] = 1;
+
+    queue<pair<int, int>> q;
 
     q.push({y, x});
 
-    while (q.size())
+    while (!q.empty())
     {
         auto cur = q.front();
         q.pop();
@@ -28,10 +29,9 @@ void bfs(int y, int x, int height)
             int ny = cur.first + dy[i];
             int nx = cur.second + dx[i];
 
-            if (ny < 0 || nx < 0 || ny >= n || nx >= n || area[ny][nx] <= height)
+            if (ny < 0 || nx < 0 || ny >= n || nx >= n)
                 continue;
-
-            if (visited[ny][nx])
+            if (visited[ny][nx] || arr[ny][nx] <= height)
                 continue;
 
             visited[ny][nx] = 1;
@@ -51,36 +51,37 @@ int main()
     {
         for (int j = 0; j < n; j++)
         {
-            int height = 0;
-            cin >> height;
+            cin >> arr[i][j];
 
-            if (height > max_height)
-                max_height = height;
-
-            area[i][j] = height;
+            if (arr[i][j] > m_height)
+            {
+                m_height = arr[i][j];
+            }
         }
     }
 
-    for (int k = 0; k < max_height; k++)
+    for (int k = 0; k < m_height; k++)
     {
         memset(visited, 0, sizeof(visited));
-        area_point = 0;
+
+        int save = 0;
+
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                if (area[i][j] > k && visited[i][j] == 0)
+                if (arr[i][j] > k && visited[i][j] == 0)
                 {
                     bfs(i, j, k);
-                    area_point++;
+                    save++;
                 }
             }
         }
-        if (area_point > ret)
-            ret = area_point;
+        if (save > ret)
+            ret = save;
     }
 
-    cout << ret << '\n';
+    cout << ret;
 
     return 0;
 }
