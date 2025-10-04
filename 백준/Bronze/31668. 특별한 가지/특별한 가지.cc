4c49f599d30eb586096d3cd81a4f3029dc7217c4
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-10-04 21:45:27 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    int result = (M / N) * K;
+    cout << result << endl;
+
+    return 0;
+}
