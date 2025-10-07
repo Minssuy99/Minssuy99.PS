a864474e123d70a12ced039855b6d7c7821fd542
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-10-08 01:33:09 +0900

    [Bronze III] Title: 모험의 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31789. 모험의 시작/모험의 시작.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#define MAX 101
+
+using namespace std;
+
+int N;
+int X, S;
+int weaponPower, weaponPrice;
+
+int main()
+{
+    cin >> N;
+    cin >> X >> S;
+    bool result = false;
+
+    for (int i = 0; i < N; i++) {
+        cin >> weaponPrice >> weaponPower;
+        if (weaponPrice <= X && weaponPower > S)
+        {
+            result = true;
+            break;
+        }
+    }
+
+    if (result) cout << "YES";
+    else cout << "NO";
+
+    return 0;
+}
