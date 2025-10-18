2964b9264ec19b7222322171b654cd2a229bd569
김민성 <101568505+Minssuy99@users.noreply.github.com>
2025-10-19 06:03:19 +0900

    [Bronze IV] Title: Quick Estimates, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string a;
+int n;
+ 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    while (n--)
+    {
+        cin >> a;
+        cout << a.size() << '\n';
+    }
+    return 0;
+}
